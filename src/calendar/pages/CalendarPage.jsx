c63b00a18dfe9758e18043cde070a75250870eb0
Mauro Leiva <leivam.mauro@gmail.com>
2024-01-25 19:21:23 -0300

    Added connection with backend

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
@@ -12,19 +12,24 @@ import {
 import { localizer, getMessagesES } from '../../helpers';
 import { useUiStore } from '../../hooks/useUiStore';
 import { useCalendarStore } from '../../hooks/useCalendarStore';
+import { useAuthStore } from '../../hooks';
 
 export const CalendarPage = () => {
     const { openDateModal } = useUiStore();
+    const { user } = useAuthStore();
 
-    const { events, setActiveEvent } = useCalendarStore();
+    const { events, setActiveEvent, startLoadingEvents } = useCalendarStore();
 
     const [lastView, setLastView] = useState(
         localStorage.getItem('lastView') || 'week'
     );
 
     const eventStyleGetter = (event, start, end, isSelected) => {
+        const isEventMine =
+            user.uid === event.user._id || user.uid === event.user.uid;
+
         const style = {
-            backgroundColor: '#347CF7',
+            backgroundColor: isEventMine ? '#347CF7' : '#465660',
             borderRadius: '0px',
             opacity: 0.8,
             color: 'white',
@@ -46,6 +51,10 @@ export const CalendarPage = () => {
     const onViewChanged = (event) => {
         localStorage.setItem('lastView', event);
     };
+
+    useEffect(() => {
+        startLoadingEvents();
+    }, []);
 
     return (
         <>
--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -1,22 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { addHours } from 'date-fns';
-
-const tempEvent = {
-    _id: new Date().getTime(),
-    title: 'Cumpleaños',
-    notes: 'Happry Verde',
-    start: new Date(),
-    end: addHours(new Date(), 2),
-    user: {
-        _id: '123',
-        name: 'Mauro',
-    },
-};
 
 export const calendarSlice = createSlice({
     name: 'calendar',
     initialState: {
-        events: [tempEvent],
+        isLoadingEvents: true,
+        events: [],
         activeEvent: null,
     },
     reducers: {
@@ -29,7 +17,7 @@ export const calendarSlice = createSlice({
         },
         onUpdateEvent: (state, { payload }) => {
             state.events = state.events.map((event) => {
-                if (event._id === payload._id) {
+                if (event.id === payload.id) {
                     return payload;
                 }
                 return event;
@@ -38,10 +26,27 @@ export const calendarSlice = createSlice({
         onDeleteEvent: (state) => {
             if (state.activeEvent) {
                 state.events = state.events.filter(
-                    (event) => event._id !== state.activeEvent._id
+                    (event) => event.id !== state.activeEvent.id
                 );
                 state.activeEvent = null;
             }
+        },
+        onLoadEvents: (state, { payload = [] }) => {
+            state.isLoadingEvents = false;
+            payload.forEach((event) => {
+                const exists = state.events.some(
+                    (dbEvent) => dbEvent.id === event.id
+                );
+
+                if (!exists) {
+                    state.events.push(event);
+                }
+            });
+        },
+        onLogoutCalendar: (state) => {
+            state.isLoadingEvents = true;
+            state.events = [];
+            state.activeEvent = null;
         },
     },
 });
@@ -52,4 +57,6 @@ export const {
     onAddNewEvent,
     onUpdateEvent,
     onDeleteEvent,
+    onLoadEvents,
+    onLogoutCalendar,
 } = calendarSlice.actions;
